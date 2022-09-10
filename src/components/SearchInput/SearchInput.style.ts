167d924b86dae4f35d4412c7514e66237bc1235b
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-09-10 15:16:35 -0300

    refactor(searchbar): alterções na estilização da searchbar

--- src/components/SearchInput/SearchInput.style.ts
+++ src/components/SearchInput/SearchInput.style.ts
@@ -4,6 +4,7 @@ export const Container = styled.input`
 /* Para compensar a outline */
 margin-top: 2px; 
 
+box-sizing: initial;
 height: 2rem;
 outline: 2px solid var(--pink-800);
 border: 4px solid var(--gray-800);
